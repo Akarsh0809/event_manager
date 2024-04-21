5abb40bcd2f9a6c0c8bfe342123ce9e1b92c3829
Akarsh0809 <157727440+Akarsh0809@users.noreply.github.com>
2024-04-20 22:09:06 -0400

    Update test_users_api.py

--- tests/test_api/test_users_api.py
+++ tests/test_api/test_users_api.py
@@ -39,6 +39,13 @@ async def test_retrieve_user(async_client, user, token):
     assert response.json()["id"] == str(user.id)
 
 @pytest.mark.asyncio
+async def test_retrieve_user2(async_client, user, token):
+    headers = {"Authorization": f"Bearer {token}"}
+    response = await async_client.get(f"/users/{user.id}", headers=headers)
+    assert response.status_code == 200
+    assert response.json()["username"] == str(user.username)
+
+@pytest.mark.asyncio
 async def test_update_user(async_client, user, token):
     updated_data = {"email": f"updated_{user.id}@example.com"}
     headers = {"Authorization": f"Bearer {token}"}
@@ -46,6 +53,15 @@ async def test_update_user(async_client, user, token):
     assert response.status_code == 200
     assert response.json()["email"] == updated_data["email"]
 
+@pytest.mark.asyncio
+async def test_update_user2(async_client, user, token):
+    updated_data = {"email": f"updated_{user.id}@example.com","bio": "I am a senior ."}
+    headers = {"Authorization": f"Bearer {token}"}
+    response = await async_client.put(f"/users/{user.id}", json=updated_data, headers=headers)
+    assert response.status_code == 200
+    assert response.json()["email"] == updated_data["email"]
+    assert response.json()["bio"] == updated_data["bio"]
+    
 @pytest.mark.asyncio
 async def test_delete_user(async_client, user, token):
     headers = {"Authorization": f"Bearer {token}"}
